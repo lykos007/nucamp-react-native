7a3dacc809c62d70f6a79b41a6ebc9c07b9bd367
Leo Kontos <lykos007@yahoo.com>
2023-05-10 17:05:48 -0500

    Persist Redux Store

--- redux/store.js
+++ redux/store.js
@@ -1,16 +1,48 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
 import { favoritesReducer } from '../features/favorites/favoritesSlice';
+import {
+    persistStore,
+    persistCombineReducers,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER
+} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const config = {
+    key: 'root',
+    storage: AsyncStorage,
+    debug: true
+};
 
 export const store = configureStore({
-    reducer: {
+    reducer: persistCombineReducers(config, {
         campsites: campsitesReducer,
         comments: commentsReducer,
         partners: partnersReducer,
         promotions: promotionsReducer,
         favorites: favoritesReducer
-    }
+    }),
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoreActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER
+                ]
+            }
+        })
 });
+
+export const persistor = persistStore(store);
