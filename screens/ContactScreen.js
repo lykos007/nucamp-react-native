6df8a48d9a4d2a0ec76284f7aea109444e454945
Leo Kontos <lykos007@yahoo.com>
2023-05-18 09:22:36 -0500

    Sending Email

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,8 +1,17 @@
-import { ScrollView, Text } from "react-native";
-import { Card } from "react-native-elements";
+import { ScrollView, Text } from 'react-native';
+import { Card, Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
 const ContactScreen = () => {
+    const sendMail = () => {
+        MailComposer.composeAsync({
+            recipients: ['campsites@nucamp.co'],
+            subject: 'Inquiry',
+            body: 'To whom it may concern:'
+        });
+    };
+
     return (
         <ScrollView>
             <Animatable.View
@@ -11,15 +20,27 @@ const ContactScreen = () => {
                 delay={1000}
             >
                 <Card wrapperStyle={{ margin: 20 }}>
-                    <Card.Title>
-                        Contact Information
-                    </Card.Title>
+                    <Card.Title>Contact Information</Card.Title>
                     <Card.Divider />
                     <Text>1 Nucamp Way</Text>
                     <Text>Seattle, WA 98001</Text>
                     <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
                     <Text>Phone: 1-206-555-1234</Text>
                     <Text>Email: campsites@nucamp.co</Text>
+                    <Button
+                        title='Send Email'
+                        buttonStyle={{ backgroundColor: '#5637DD', margin: 40 }}
+                        icon={
+                            <Icon
+                                name='envelope-o'
+                                type='font-awesome'
+                                color='#fff'
+                                iconStyle={{ marginRight: 10 }}
+                            />
+                        }
+                        onPress={() => sendMail()
+                        }
+                    />
                 </Card>
             </Animatable.View>
         </ScrollView>
